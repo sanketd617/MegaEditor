2da2a065491d862c4f8bc340fa3928ccbaf24b9b
Sanket <dahegaonkarsanket>
2019-01-01 11:17:17 +0530

    Fixed bugs

--- MegaEditor.py
+++ MegaEditor.py
@@ -14,59 +14,73 @@ class MegaEditor:
         self.window.refresh()
         self.lines = [""]
 
-    def setCursorPos(self):
-        self.curr_y, self.curr_x = self.window.getyx()
-
     def write(self, c):
-        self.setCursorPos()
-
         if ord(c) == 10:
             part_one = self.lines[self.curr_y][:self.curr_x]
             part_two = self.lines[self.curr_y][self.curr_x:]
             self.lines = self.lines[:self.curr_y+1] + [part_two] + self.lines[self.curr_y+1:]
             self.lines[self.curr_y] = part_one
-            self.window.addstr(self.curr_y, 0, part_one + (" "*len(part_two)))
-            self.window.move(self.curr_y+1, 0)
-            self.window.insertln()
-            self.window.addstr(part_two)
-            self.curr_y += 1
             self.curr_x = 0
+            self.curr_y += 1
         else:
-            self.window.addstr(c)
+            self.lines[self.curr_y] = self.lines[self.curr_y][:self.curr_x] + c + self.lines[self.curr_y][self.curr_x:]
             self.curr_x += 1
-            self.lines[self.curr_y] += c
-        self.window.move(self.curr_y, self.curr_x)
 
+        self.write_lines()
+        self.window.refresh()
+
+    def write_lines(self):
+        ty = 0
+        self.window.clear()
+        for line in self.lines:
+            self.window.addstr(ty, 0, line)
+            ty += 1
+
+        self.window.move(self.curr_y, self.curr_x)
         self.window.refresh()
 
     def backspace(self):
-        self.setCursorPos()
-
         if self.curr_x == 0 and self.curr_y != 0:
             part_one = self.lines[self.curr_y-1]
             part_two = self.lines[self.curr_y]
             self.lines = self.lines[:self.curr_y] + self.lines[self.curr_y+1:]
             self.lines[self.curr_y-1] = part_one + part_two
-            self.window.addstr(self.curr_y-1, 0, part_one + part_two)
-            self.window.move(self.curr_y, 0)
-            self.window.deleteln()
             self.curr_y -= 1
             self.curr_x = len(part_one)
-        elif self.curr_y != 0:
+        elif self.curr_x == 0 and self.curr_y == 0:
+            return False
+        elif self.curr_x == len(self.lines[self.curr_y]):
             self.curr_x -= 1
-            self.window.delch(self.curr_y, self.curr_x)
-            self.lines[self.curr_y] += self.lines[self.curr_y][:self.curr_x]+self.lines[self.curr_y][self.curr_x+1:]
-        self.window.move(self.curr_y, self.curr_x)
+            self.lines[self.curr_y] = self.lines[self.curr_y][:self.curr_x]
+        else:
+            self.curr_x -= 1
+            temp = self.lines[self.curr_y]
+            part_one = temp[:self.curr_x]
+            part_two = temp[self.curr_x+1:]
+            self.lines[self.curr_y] = part_one + part_two
 
-        self.window.refresh()
+        self.write_lines()
 
     def delete(self):
+        if self.curr_x == len(self.lines[self.curr_y]) and self.curr_y != len(self.lines)-1:
+            part_one = self.lines[self.curr_y]
+            part_two = self.lines[self.curr_y+1]
+            self.lines = self.lines[:self.curr_y] + self.lines[self.curr_y+1:]
+            self.lines[self.curr_y] = part_one + part_two
+            self.curr_x = len(part_one)
+        elif self.curr_x == len(self.lines[self.curr_y]) and self.curr_y == len(self.lines)-1:
+            return False
+        elif self.curr_x == 0:
+            self.lines[self.curr_y] = self.lines[self.curr_y][:self.curr_x]
+        else:
+            temp = self.lines[self.curr_y]
+            part_one = temp[:self.curr_x]
+            part_two = temp[self.curr_x+1:]
+            self.lines[self.curr_y] = part_one + part_two
 
-        self.window.refresh()
+        self.write_lines()
 
     def moveLeft(self):
-        self.setCursorPos()
-
         if self.curr_x == 0:
             if self.curr_y == 0:
                 return False
@@ -79,8 +93,6 @@ class MegaEditor:
         self.window.refresh()
 
     def moveRight(self):
-        self.setCursorPos()
-
         if self.curr_x == len(self.lines[self.curr_y]):
             if self.curr_y == len(self.lines)-1:
                 return False
@@ -93,8 +105,6 @@ class MegaEditor:
         self.window.refresh()
 
     def moveUp(self):
-        self.setCursorPos()
-
         if self.curr_y == 0:
             return False
         self.curr_y -= 1
@@ -104,8 +114,6 @@ class MegaEditor:
         self.window.refresh()
 
     def moveDown(self):
-        self.setCursorPos()
-
         if self.curr_y == len(self.lines)-1:
             return False
         self.curr_y += 1
