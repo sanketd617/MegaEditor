65bd23ad766e34d9c3fdcc1aaf56c30091dcce2c
Sanket <dahegaonkarsanket>
2019-01-05 11:30:38 +0530

    y-scrolling working great..!!

--- MegaEditor.py
+++ MegaEditor.py
@@ -14,24 +14,30 @@ class MegaEditor:
         self.window.refresh()
         self.lines = [""]
         self.file = file
+        self.start_y = 0
+        self.line_index = 0
 
     def resize(self, y, x):
         self.window.resize(y, x)
 
     def write(self, c):
+        self.line_index = self.start_y + self.curr_y
         if ord(c) == 10:
-            part_one = self.lines[self.curr_y][:self.curr_x]
-            part_two = self.lines[self.curr_y][self.curr_x:]
-            self.lines = self.lines[:self.curr_y+1] + [part_two] + self.lines[self.curr_y+1:]
-            self.lines[self.curr_y] = part_one
+            part_one = self.lines[self.line_index][:self.curr_x]
+            part_two = self.lines[self.line_index][self.curr_x:]
+            self.lines = self.lines[:self.line_index+1] + [part_two] + self.lines[self.line_index+1:]
+            self.lines[self.line_index] = part_one
             self.curr_x = 0
-            self.curr_y += 1
+            if self.curr_y < self.maxy-1:
+                self.curr_y += 1
+            else:
+                self.start_y += 1
         else:
             if ord(c) == 9:
-                self.lines[self.curr_y] = self.lines[self.curr_y][:self.curr_x] + "    " + self.lines[self.curr_y][self.curr_x:]
+                self.lines[self.line_index] = self.lines[self.line_index][:self.curr_x] + "    " + self.lines[self.line_index][self.curr_x:]
                 self.curr_x += 4
             else:
-                self.lines[self.curr_y] = self.lines[self.curr_y][:self.curr_x] + c + self.lines[self.curr_y][self.curr_x:]
+                self.lines[self.line_index] = self.lines[self.line_index][:self.curr_x] + c + self.lines[self.line_index][self.curr_x:]
                 self.curr_x += 1
 
         self.write_lines()
@@ -40,7 +46,11 @@ class MegaEditor:
     def write_lines(self):
         ty = 0
         self.window.clear()
-        for line in self.lines:
+        end_y = self.start_y + self.maxy
+        if end_y > len(self.lines):
+            end_y = len(self.lines)
+        lines = self.lines[self.start_y:end_y]
+        for line in lines:
             self.window.addstr(ty, 0, line)
             ty += 1
 
@@ -48,87 +58,110 @@ class MegaEditor:
         self.window.refresh()
 
     def backspace(self):
-        if self.curr_x == 0 and self.curr_y != 0:
-            part_one = self.lines[self.curr_y-1]
-            part_two = self.lines[self.curr_y]
-            self.lines = self.lines[:self.curr_y] + self.lines[self.curr_y+1:]
-            self.lines[self.curr_y-1] = part_one + part_two
-            self.curr_y -= 1
+        self.line_index = self.start_y + self.curr_y
+        if self.curr_x == 0 and self.line_index != 0:
+            part_one = self.lines[self.line_index-1]
+            part_two = self.lines[self.line_index]
+            self.lines = self.lines[:self.line_index] + self.lines[self.line_index+1:]
+            self.lines[self.line_index-1] = part_one + part_two
+            if self.curr_y > 0:
+                self.curr_y -= 1
+            else:
+                self.start_y -= 1
             self.curr_x = len(part_one)
-        elif self.curr_x == 0 and self.curr_y == 0:
+        elif self.curr_x == 0 and self.line_index == 0:
             return False
-        elif self.curr_x == len(self.lines[self.curr_y]):
+        elif self.curr_x == len(self.lines[self.line_index]):
             self.curr_x -= 1
-            self.lines[self.curr_y] = self.lines[self.curr_y][:self.curr_x]
+            self.lines[self.line_index] = self.lines[self.line_index][:self.curr_x]
         else:
             self.curr_x -= 1
-            temp = self.lines[self.curr_y]
+            temp = self.lines[self.line_index]
             part_one = temp[:self.curr_x]
             part_two = temp[self.curr_x+1:]
-            self.lines[self.curr_y] = part_one + part_two
+            self.lines[self.line_index] = part_one + part_two
 
         self.write_lines()
 
     def delete(self):
-        if self.curr_x == len(self.lines[self.curr_y]) and self.curr_y != len(self.lines)-1:
-            part_one = self.lines[self.curr_y]
-            part_two = self.lines[self.curr_y+1]
-            self.lines = self.lines[:self.curr_y] + self.lines[self.curr_y+1:]
-            self.lines[self.curr_y] = part_one + part_two
+        self.line_index = self.start_y + self.curr_y
+        if self.line_index == len(self.lines[self.line_index]) and self.line_index != len(self.lines)-1:
+            part_one = self.lines[self.line_index]
+            part_two = self.lines[self.line_index+1]
+            self.lines = self.lines[:self.line_index] + self.lines[self.line_index+1:]
+            self.lines[self.line_index] = part_one + part_two
             self.curr_x = len(part_one)
-        elif self.curr_x == len(self.lines[self.curr_y]) and self.curr_y == len(self.lines)-1:
+        elif self.curr_x == len(self.lines[self.line_index]) and self.line_index == len(self.lines)-1:
             return False
-        elif self.curr_x == 0:
-            self.lines[self.curr_y] = self.lines[self.curr_y][:self.curr_x]
+        # elif self.curr_x == 0:
+        #     self.lines[self.curr_y] = self.lines[self.curr_y][:self.curr_x]
         else:
-            temp = self.lines[self.curr_y]
+            temp = self.lines[self.line_index]
             part_one = temp[:self.curr_x]
             part_two = temp[self.curr_x+1:]
-            self.lines[self.curr_y] = part_one + part_two
+            self.lines[self.line_index] = part_one + part_two
 
         self.write_lines()
 
     def moveLeft(self):
+        self.line_index = self.start_y + self.curr_y
         if self.curr_x == 0:
-            if self.curr_y == 0:
+            if self.line_index == 0:
                 return False
             else:
-                self.curr_y -= 1
-                self.curr_x = len(self.lines[self.curr_y])
+                if self.curr_y > 0:
+                    self.curr_y -= 1
+                else:
+                    self.start_y -= 1
+                self.line_index = self.start_y + self.curr_y
+                self.curr_x = len(self.lines[self.line_index])
         else:
             self.curr_x -= 1
-        self.window.move(self.curr_y, self.curr_x)
-        self.window.refresh()
+
+        self.write_lines()
 
     def moveRight(self):
-        if self.curr_x == len(self.lines[self.curr_y]):
-            if self.curr_y == len(self.lines)-1:
+        self.line_index = self.start_y + self.curr_y
+        if self.curr_x == len(self.lines[self.line_index]):
+            if self.line_index == len(self.lines)-1:
                 return False
             else:
-                self.curr_y += 1
+                if self.curr_y < self.maxy - 1:
+                    self.curr_y += 1
+                else:
+                    self.start_y += 1
                 self.curr_x = 0
         else:
             self.curr_x += 1
-        self.window.move(self.curr_y, self.curr_x)
-        self.window.refresh()
+
+        self.write_lines()
 
     def moveUp(self):
-        if self.curr_y == 0:
+        self.line_index = self.start_y + self.curr_y
+        if self.line_index <= 0:
             return False
-        self.curr_y -= 1
-        if self.curr_x > len(self.lines[self.curr_y-1]):
-            self.curr_x = len(self.lines[self.curr_y-1])
-        self.window.move(self.curr_y, self.curr_x)
-        self.window.refresh()
+
+        if self.curr_y > 0:
+            self.curr_y -= 1
+        else:
+            self.start_y -= 1
+
+        if self.curr_x > len(self.lines[self.line_index-1]):
+            self.curr_x = len(self.lines[self.line_index-1])
+
+        self.write_lines()
 
     def moveDown(self):
-        if self.curr_y == len(self.lines)-1:
+        self.line_index = self.start_y + self.curr_y
+        if self.line_index == len(self.lines)-1:
             return False
-        self.curr_y += 1
-        if self.curr_x > len(self.lines[self.curr_y]):
-            self.curr_x = len(self.lines[self.curr_y])
-        self.window.move(self.curr_y, self.curr_x)
-        self.window.refresh()
+        if self.curr_y < self.maxy-1:
+            self.curr_y += 1
+        else:
+            self.start_y += 1
+        if self.curr_x > len(self.lines[self.line_index]):
+            self.curr_x = len(self.lines[self.line_index])
+        self.write_lines()
 
     def save(self):
         file = open(self.file, "w+")
--- MegaLineNums.py
+++ MegaLineNums.py
@@ -4,8 +4,8 @@ import curses.panel
 
 class MegaLineNums:
 
-    def __init__(self, maxy, n = 1):
-        self.num_lines = 0
+    def __init__(self, maxy, start = 1, end = 1):
+        self.num_lines = start-1
         self.window = curses.newwin(maxy, 3, 3, 0)
 
         self.BG = 241
@@ -15,8 +15,11 @@ class MegaLineNums:
         curses.use_default_colors()
         curses.init_pair(self.COLOR_PAIR, self.BG, self.FG)
 
-        for i in range(1, n+1):
-            self.add()
+        t = min(start + end, maxy)
+
+        for i in range(start, t):
+            self.num_lines += 1
+            self.window.addstr((i-start), 0, (" "*(3-len(str(self.num_lines))))+str(self.num_lines), curses.color_pair(self.COLOR_PAIR))
 
         for i in range(self.num_lines+1, maxy):
             self.window.addstr("   ", curses.color_pair(2))
